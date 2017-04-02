c4766e5e149acca6412ab2547d400cf63b8dfa98
uladbohdan <vlbohdan@gmail.com>
2017-04-03 01:10:23 +0300

    simplyfing grep regexp (excluding any build folders)

--- cpplint.sh
+++ cpplint.sh
@@ -3,4 +3,4 @@ cpplint \
     --filter=-build/include_subdir,-build/include_order \
     --linelength=80 \
     --counting=detailed \
-$(find . -name "*.cpp" -or -name "*.h" | grep -vE "^./libs/")
+$(find . -name "*.cpp" -or -name "*.h" | grep -vE "(libs|build)")
